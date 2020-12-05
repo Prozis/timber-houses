ecd8a46dcd2d6b4d9b7a81493b38ae8c5bac17f7
Prozis <prozius@tut.by>
2020-12-05 13:14:25 +0300

    сделана верстка

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Дома из бруса</title>
+  <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+  <header class="header">
+    <div class="header_inner">
+      <img src="img/home.png" alt="">
+      <div class="header_name">
+        Timber House
+
+      </div>
+      <a href="tel:+0930909234" class="phone">+7(035) 277-00-00</a>
+      <div class="header_title">
+        Дома из бруса
+        под ключ
+      </div>
+      <div class="header_sale">
+        <img src="img/sale.png" alt="">
+
+      </div>
+    </div>
+
+  </header>
+
+  <main class="main">
+    <div class="main_title">
+      Строительство домов и бань из
+      строганного и профилированного бруса
+    </div>
+    <div class="main_text">
+      За 10 лет мы возвели больше 250 домов и бань из бруса
+      ИМЕЕМ БОЛЬШОЙ ОПЫТ В СТРОИТЕЛЬСТВЕ
+      гарантируем качество и надежность всех построенных нами объектов
+
+      ГАРАНТИЯ НА ДОМА И БАНИ ИЗ БРУСА 3 ГОДА
+
+    </div>
+    <div class="main_img">
+      <img src="img/main-img.png" alt="">
+
+    </div>
+
+    <div class="projects">
+
+      <div class="project_item">
+        <div class="project_title">
+          ПРОЕКТЫ ДОМОВ ИЗ БРУСА
+        </div>
+
+          <div class="project_name">
+            ДОМ #1
+          </div>
+          <div class="project_size">
+            Размер дома: 6 x 6
+          </div>
+          <div class="project_area">
+            Общая площадь дома: 54 кв. м
+          </div>
+          <div class="project_price">
+            Стоимость - 402 000 рублей
+          </div>
+          <div class="project_images">
+            <div class="project_images_item">
+              <img src="/img/project-1.ipg" alt="">
+            </div>
+            <div class="project_images_item">
+              <img src="/img/project-2.ipg" alt="">
+            </div>
+          </div>
+          </div>
+      </div>
+      <div class="download">
+        <img src="img/home-downloads.png" alt="">
+        <a href="#">СКАЧАТЬ ВЕСЬ КАТАЛОГ ДОМОВ</a>
+      </div>
+      <div class="gallery">
+        <div class="gallery_title">
+          ФОТОГРАФИИ НАШИХ РАБОТ
+        </div>
+        <div class="gallery_text">
+          некоторые фотографии построенных нами домов и бань из бруса
+        </div>
+        <div class="gallery_inner">
+          <a href="img/item-1.jpg"><img src="img/item.jpg" alt=""> </a>
+          <a href="img/item-1.jpg"><img src="img/item.jpg" alt=""> </a>
+          <a href="img/item-1.jpg"><img src="img/item.jpg" alt=""> </a>
+          <a href="img/item-1.jpg"><img src="img/item.jpg" alt=""> </a>
+          <a href="img/item-1.jpg"><img src="img/item.jpg" alt=""> </a>
+          <a href="img/item-1.jpg"><img src="img/item.jpg" alt=""> </a>
+          <a href="img/item-1.jpg"><img src="img/item.jpg" alt=""> </a>
+          <a href="img/item-1.jpg"><img src="img/item.jpg" alt=""> </a>
+          <a href="img/item-1.jpg"><img src="img/item.jpg" alt=""> </a>
+          <a href="img/item-1.jpg"><img src="img/item.jpg" alt=""> </a>
+          <a href="img/item-1.jpg"><img src="img/item.jpg" alt=""> </a>
+          <a href="img/item-1.jpg"><img src="img/item.jpg" alt=""> </a>
+        </div>
+      </div>
+      <div class="main_img">
+        <img src="img/main-img.png" alt="">
+
+      </div>
+
+  </main>
+  <footer class="footer">
+    <a href="tel:+0930909234" class="phone">+7(035) 277-00-00</a>
+    <div class="footer_email">
+      namename@gmail.com
+    </div>
+  </footer>
+</body>
+</html>
